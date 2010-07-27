c06ea623944e4a6485a3397df261886bc0ab8ffb
brian mulloy <brian@landlessness.net>
2010-07-26 22:34:58 -0700

    first pass at tags using acts_as_taggable_on. TODO: make tags linkable to a list of thoughts scoped to tag and optionally project.

--- Gemfile
+++ Gemfile
@@ -31,3 +31,4 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 # end
 
 gem 'devise', '1.1.rc2'
+gem 'acts-as-taggable-on'
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,4 +1,7 @@
 class Thought < ActiveRecord::Base
   belongs_to :person
   belongs_to :project
+
+  acts_as_taggable
+
 end
