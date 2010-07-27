aa5157063619da8f46133e6036f62acd7164c47b
brian mulloy <brian@landlessness.net>
2010-07-27 14:30:15 -0700

    added description to project

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100727052126) do
+ActiveRecord::Schema.define(:version => 20100727212545) do
 
   create_table "memberships", :force => true do |t|
     t.integer  "person_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20100727052126) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "taggings", :force => true do |t|
