958f32aa104dd157f192080f887dbb0f802088fc
brian mulloy <brian@landlessness.net>
2010-07-30 01:30:44 -0700

    the association code for creating has_one drop box did not work the way i expected. so had to change it.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,10 +2,10 @@ class Person < ActiveRecord::Base
   has_many :thoughts
   has_many :memberships, :dependent => :destroy
   has_many :projects, :through => :memberships
-  has_one :drop_box
+  has_one :drop_box, :dependent => :destroy
   
   after_create do
-    self.drop_box.create :name => self.email.split('@').first, :secret => rand(9999)
+    self.create_drop_box :name => self.email.split('@').first, :secret => rand(9999)
   end
 
   # Include default devise modules. Others available are:
