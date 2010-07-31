62f7528dea2973f2f2279f9809356b1cb089d8fc
brian mulloy <brian@landlessness.net>
2010-07-31 15:51:31 -0700

    fixed a bug in the drop box. seemed to be caused by the strip! command

--- app/models/drop_box.rb
+++ app/models/drop_box.rb
@@ -7,55 +7,30 @@ class DropBox < ActiveRecord::Base
   end
   
   def self.new_thought(send_grid_mail)
-    t = nil
-    name, secret = name_and_secret_from_email(send_grid_mail[:to])
-    d = DropBox.find_by_name_and_secret(name, secret)
+    project = nil
     
-    unless d.nil?
-      project = nil
-      unless send_grid_mail[:subject].nil? || send_grid_mail[:subject].empty?
-        send_grid_mail[:subject].strip!
-        project = d.person.projects.find_by_name(send_grid_mail[:subject])
-        
-        if project.nil?
-          project = Project.new(:name => send_grid_mail[:subject])
-          
-          membership = d.person.memberships.build :project => project
-        end
-      end
-      t = d.person.thoughts.build :body => send_grid_mail[:text].strip!, :project => project
+    name, secret = name_and_secret_from_email(send_grid_mail[:to])
+    d = DropBox.find_by_name_and_secret(name, secret)    
+    
+    if d.nil? || send_grid_mail[:text].nil?
+      return nil
+    else
+      send_grid_mail[:text].strip!
     end
-    t
+
+    unless send_grid_mail[:subject].nil? || send_grid_mail[:subject].empty?
+      send_grid_mail[:subject].strip!
+      project = d.person.projects.find_by_name(send_grid_mail[:subject])
+      if project.nil?
+        project = Project.new(:name => send_grid_mail[:subject])
+
+        membership = d.person.memberships.build :project => project
+      end
+    end
+
+    d.person.thoughts.build :body => send_grid_mail[:text], :project => project
   end
 
-  def self.process_email(mail)
-    name, secret = mail.to.first.split('@').first.split('+')
-    d = DropBox.find_by_name_and_secret(name, secret)
-    body = ''
-    if mail.parts.empty?
-      body = mail.body.to_s.strip
-    else
-      mail.parts.each do |part|
-        body = part.body.to_s.strip if part.content_type.include?('text/plain')
-      end
-    end
-    project = nil
-    unless mail.subject.nil? || mail.subject.empty?
-      project = d.person.projects.where('name like ?', mail.subject).first
-      if project.nil?
-        project = Project.new(:name => mail.subject)
-        membership = d.person.memberships.new
-        membership.project = @project
-        project.save!
-        membership.save!
-      end
-    end
-    t = d.person.thoughts.create! :body => body, :project => project
-    t.put_in_drop_box
-    raise "thought, #{t.body}, was not created for person, #{d.person.email}. errors: #{t.errors.full_messages.to_s}" if t.nil?
-    t
-  end
-  
   def email
     self.name + '+' + self.secret + '@crumpleit.com'
   end
--- test/functional/thoughts_controller_test.rb
+++ test/functional/thoughts_controller_test.rb
@@ -43,6 +43,21 @@ class ThoughtsControllerTest < ActionController::TestCase
     assert_response :ok
   end
 
+  test "should create thought from html email" do    
+    @send_grid_html_mail = send_grid_html_mail
+    
+    assert @send_grid_html_mail[:to] && @send_grid_html_mail[:from]
+    assert_difference('Thought.count') do
+      post :create_from_sendgrid, @send_grid_html_mail.merge(:format => 'xml')
+    end
+    thought = assigns(:thought)
+    assert_equal "\"I very rarely think in words at all. A thought comes, and I may try to express it in words afterwards,\" -Albert Einstein (Wertheimer, 1959, 213; Pais, 1982). \n\nhttp://www.psychologytoday.com/blog/imagine/201003/einstein-creative-thinking-music-and-the-intuitive-art-scientific-imagination", thought.body
+    
+    assert_equal 'drop_box', thought.state
+    
+    assert_response :ok
+  end
+
   test "should show thought" do
     get :show, :id => @thought.to_param
     assert_response :success
