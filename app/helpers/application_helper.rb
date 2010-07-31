dc9819a5198ff5945d6d503fed4da9dd12602671
brian mulloy <brian@landlessness.net>
2010-07-31 14:54:06 -0700

    make whole drop box link bold when there are thoughts in it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def link_to_drop_box
-    drop_box_text = 'Drop Box' + (@drop_box_count > 0 ? " <b>(#{@drop_box_count})</b>" : '')
+    drop_box_text = 'Drop Box'
+    drop_box_text = '<b>' + drop_box_text + " (#{@drop_box_count})</b>" if @drop_box_count > 0
     link_to(drop_box_text.html_safe, drop_box_thoughts_path).html_safe
   end
 end
