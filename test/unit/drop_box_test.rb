a193d58787fe39398373cc73db1323ec8cb654dc
brian mulloy <brian@landlessness.net>
2010-07-31 18:38:52 -0700

    added a test for utf 8 in drop box. the test passes, but production error on http://crumpleit.hoptoadapp.com/errors/2133020 ActiveRecord::StatementInvalid: PGError: ERROR: invalid byte sequence for encoding "UTF8"

--- test/unit/drop_box_test.rb
+++ test/unit/drop_box_test.rb
@@ -74,6 +74,42 @@ class DropBoxTest < ActiveSupport::TestCase
     assert !thought.project.new_record?
   end
   
+  test 'find person and create new thought via drop box with utf8 email' do
+    @send_grid_utf8_mail = send_grid_utf8_mail
+    
+    assert_equal 1, Person.count
+    assert_equal 2, DropBox.count
+    person = Person.first
+    
+    assert_not_nil @send_grid_utf8_mail[:to]
+    assert_equal projects(:crumple).name, @send_grid_utf8_mail[:subject].strip!
+    project = person.projects.find_by_name(@send_grid_utf8_mail[:subject])
+    assert_not_nil project
+    assert_equal projects(:crumple), project
+    drop_box_name, drop_box_secret = DropBox.name_and_secret_from_email(@send_grid_utf8_mail[:to])
+    assert_equal drop_boxes(:brians_drop_box).name, drop_box_name
+    assert_equal drop_boxes(:brians_drop_box).secret, drop_box_secret
+    
+    assert_equal person, DropBox.find_by_name_and_secret(drop_box_name, drop_box_secret).person
+
+    assert_equal drop_box_name, person.drop_box.name
+    assert_equal drop_box_secret, person.drop_box.secret
+
+    assert_not_nil @send_grid_utf8_mail[:text]
+    
+    thought = DropBox.new_thought @send_grid_utf8_mail
+    
+    assert_not_nil thought.body
+    assert_equal thought.body, @send_grid_utf8_mail[:text]
+    assert_not_nil thought.person
+    assert_equal person, thought.person
+    assert_not_nil thought.project
+    assert_equal project, thought.project
+    assert_equal @send_grid_utf8_mail[:subject], thought.project.name
+    assert @send_grid_utf8_mail[:text].include?(thought.body)
+    assert thought.new_record?, 'asserting that the thought is a new record'
+    assert !thought.project.new_record?
+  end
   
 end
 
