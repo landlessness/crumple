299302401f91d1f7725bf689e32ce373ecb7ac99
brian mulloy <brian@landlessness.net>
2010-08-02 18:52:59 -0700

    cleaned up the new thoughts link out of the thoughts listing partial and into a helper.

--- app/helpers/thoughts_helper.rb
+++ app/helpers/thoughts_helper.rb
@@ -13,6 +13,13 @@ module ThoughtsHelper
     cloud.html_safe
   end
 
+  def link_to_new_thought(project=nil)
+     if project 
+       link_to 'New thought about ' + project.name, new_project_thought_path(project)
+     else 
+       link_to 'New thought', new_person_thought_path(current_person) 
+     end 
+  end
 
   def link_to_tag(tag, css_class, project)
     link_to tag.name, link_path(tag, project), :class => css_class
