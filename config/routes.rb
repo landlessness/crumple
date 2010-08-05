0f6381af49bec120ddb863925644df1a6ff4ad7e
brian mulloy <brian@landlessness.net>
2010-08-04 20:32:43 -0700

    fixed typo in curl user agent

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Crumple::Application.routes.draw do |map|
   # must come before the resources :thoughts line
   # 
   # to test this route: 
-  # curl --data '' --header 'content-type: text/xml' -X POST --user-age 'SendGrid 1.0' 'localhost:3000/thoughts.xml?to=brian%2B4444@crumpleapp.com&text=hello%0Atags%3Acurl%0Aproject%3Atest&subject=hi'
+  # curl --data '' --header 'content-type: text/xml' -X POST --user-agent 'SendGrid 1.0' 'localhost:3000/thoughts.xml?to=brian%2B4444@crumpleapp.com&text=hello%0Atags%3Acurl%0Aproject%3Atest&subject=hi'
   
   
   match '/thoughts.:format' => 'send_grid_emails#create', :constraints => { :user_agent => /SendGrid/i, :format => 'xml' }, :via => :post
