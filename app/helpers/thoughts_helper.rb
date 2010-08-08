625eedec61a590062bd8a322a20197b30fd9e3ed
brian mulloy <brian@landlessness.net>
2010-08-08 02:12:14 -0700

    i think using Array#index with ruby objects for viz data was messing things up. now I am explicitly using an index for creating links from taggings.

--- app/helpers/thoughts_helper.rb
+++ app/helpers/thoughts_helper.rb
@@ -6,14 +6,14 @@ module ThoughtsHelper
     
     js_data = "{nodes:["
     nodes.each do |n|
-      node_name = render_html ? n.viz_html_node_name : n.viz_node_name
-      js_data += %(\n{nodeValue:"#{escape_javascript(n.viz_node_value)}", nodeName:"#{escape_javascript(node_name)}", group:#{viz_group(n,projects,project)}})
-      js_data += n == nodes.last ? '' : ','
+      node_name = render_html ? n.first.viz_html_node_name : n.first.viz_node_name
+      js_data += %(\n{nodeValue:"#{escape_javascript(n.first.viz_node_value)}", nodeName:"#{escape_javascript(node_name)}", group:#{viz_group(n.first,projects,project)}})
+      js_data += n.first == nodes.last ? '' : ','
     end
     js_data += "],\nlinks:["
     links.each do |l|
-      source = nodes.index(l.tag)
-      target = nodes.index(l.thought)
+      source = nodes.select{|n| n.first.class==Tag && n.first.id==l.tag.id}.first.last
+      target = nodes.select{|n| n.first.class==Thought && n.first.id==l.thought.id}.first.last
       next if source.nil? || target.nil?
       js_data += %(\n{source:#{source}, target:#{target}})
       js_data += l == links.last ? '' : ','
@@ -36,11 +36,8 @@ module ThoughtsHelper
   
   def viz_nodes(thoughts, tags)
     nodes = []
-    tags.each do |t|
-      nodes << t
-    end
-    thoughts.each do |t|
-      nodes << t
+    (tags+thoughts).each_with_index do |t,i|
+      nodes << [t,i]
     end
     nodes
   end
