86202ec3b44f1f08eee5851290b7861f6911c99a
brian mulloy <brian@landlessness.net>
2010-08-08 17:48:37 -0700

    some css stuff for the name and tagline

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Crumple::Application.initialize!
+
+Crumple::Application.configure do
+  config.product_name = 'Crumple'
+  config.product_tag_line = 'A notebook'
+  config.product_version = 'proto'
+end
