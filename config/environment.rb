d114d1809281c93b5568244e2ade017364d499bc
brian mulloy <brian@landlessness.net>
2010-08-09 03:18:50 -0700

    thoughts#new text area is at 100% width and looking good.

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Crumple::Application.initialize!
 
 Crumple::Application.configure do
-  config.product_name = 'Crumple'
-  config.product_tag_line = 'A notebook'
+  config.product_name = 'crumple'
+  config.product_tag_line = 'a notebook'
   config.product_version = 'proto'
 end
