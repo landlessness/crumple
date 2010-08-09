10560449b689acab2ed89a349edb08514d101700
brian mulloy <brian@landlessness.net>
2010-08-09 12:29:29 -0700

    text area auto grow thx to a plugin from gtihub

---
+++ public/javascripts/jquery.resize-font.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  if ($('.resize-font').length) {
+    $(window).resize(resize);
+    resize();
+  }
+});
+
+function resize() {
+  var newFontSize =  Math.floor((($('.resize-font').width() - 6.3799) / 25.407));  
+  $('.resize-font').css('font-size', newFontSize)
+}
+
+// copy/paste for testing
+// Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  
