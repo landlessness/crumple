7fdf0ad8e162dbbb7d79eec6f047f9e538ba4fec
brian mulloy <brian@landlessness.net>
2010-08-12 20:50:28 -0700

    i18n'd the home page. added some iPhone stuff. more contrast between white and gray link colors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def link_to_drop_box
-    drop_box_text = 'Drop Box'
+    drop_box_text = t(:drop_box)
     drop_box_text = drop_box_text + " (#{@drop_box_count})" if @drop_box_count > 0
     link_to(drop_box_text.html_safe, my_drop_box_path, :class => 'lil-space-left' + drop_box_nav_class).html_safe
   end  
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,4 @@ require File.expand_path('../application', __FILE__)
 Crumple::Application.initialize!
 
 Crumple::Application.configure do
-  config.product_name = 'crumple'
-  config.product_tag_line = 'a notebook'
-  config.product_version = 'proto'
 end
