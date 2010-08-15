7fe457a9e76c7cb1959ee192595059a4bc4050c8
brian mulloy <brian@landlessness.net>
2010-08-15 00:06:10 -0700

    forcing word wrap in tipsy with some javascript.

--- public/javascripts/jquery.auto-link.js
+++ public/javascripts/jquery.auto-link.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
   $('.auto-link').each(function(index) {
-    $(this).attr('title',$(this).attr('href'));
+    $(this).attr('title',wordwrap($(this).attr('href'),35,null,true));
     $(this).tipsy({gravity: 's'});
   });
 });
+
+function wordwrap( str, width, brk, cut ) {
+    brk = brk || '\n';
+    width = width || 75;
+    cut = cut || false;
+    if (!str) { return str; }
+    var regex = '.{1,' +width+ '}(\\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\\S+?(\\s|$)');
+    return str.match( RegExp(regex, 'g') ).join( brk );
+}
