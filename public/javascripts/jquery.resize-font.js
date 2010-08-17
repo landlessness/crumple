d3d94375178b634594f10f3ea34e3e7453f9efe5
brian mulloy <brian@landlessness.net>
2010-08-17 11:04:05 -0700

    set the lower limit on resize fonts, made action links more cascading. fixed the password input size issue.

--- public/javascripts/jquery.resize-font.js
+++ public/javascripts/jquery.resize-font.js
@@ -7,8 +7,10 @@ $(document).ready(function(){
 
 function resize() {
   var sizeCoefficient = 1.0;
+  var minSize = 20;
   $('.resize-font').each(function(index) {
     var newFontSize = Math.floor(sizeCoefficient * (0.0394 * $(this).width() + 0.2489));
+    if (newFontSize < minSize) {newFontSize = minSize};
     $(this).css('font-size', newFontSize);
   });
 }
