3a9df95357deedcf13751b8de495710974055716
brian mulloy <brian@landlessness.net>
2010-08-20 10:42:44 -0700

    removed the 10x extra loops on jquery resize. and made default font size the same as iPad so ipad doesnt blip.

--- public/javascripts/jquery.resize-font.js
+++ public/javascripts/jquery.resize-font.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
 
 function resize() {
   var sizeCoefficient = 1.0;
-  var minSize = 20;
+  var minSize = 16;
   $('.resize-font').each(function(index) {
     var newFontSize = Math.floor(sizeCoefficient * (0.0394 * $(this).width() + 0.2489));
     if (newFontSize < minSize) {newFontSize = minSize};
     $(this).css('font-size', newFontSize);
-  });
+  });    
 }
