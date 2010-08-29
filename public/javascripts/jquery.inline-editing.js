ec864c06675f82523222cd3c6db1c433deb6592b
brian mulloy <brian@landlessness.net>
2010-08-28 20:24:32 -0700

    another intermediate step for inline comment editing: turning the comment form on and off.

---
+++ public/javascripts/jquery.inline-editing.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $('.edit-comment-link').click(function () {
+    comment = $(this).parents('.comment')
+    comment_editor = comment.prev('.edit-comment');
+    comment.hide();
+    comment_editor.show();
+    comment_editor.focus();
+    resize();
+    return false;
+  })
+
+  $('.cancel-edit-comment-link').click(function () {
+    comment_editor = $(this).parents('.edit-comment');
+    comment = comment_editor.next('.comment')
+    comment.show();
+    comment_editor.hide();
+    resize();
+    return false;
+  })
+})
