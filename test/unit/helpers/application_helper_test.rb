882142e5677b532903b00eec5e6126040e88ebbc
landlessness <brian@landlessness.net>
2010-09-20 22:58:16 -0700

    tuning up for testing

---
+++ test/unit/helpers/application_helper_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test 'display text' do
+    link = 'http://www.google.com'
+    text = 'click this ' + link
+    assert_equal "click this <a class=\"auto-link\" href=\"http://www.google.com\">http://www.google.com</a>", display_text(text)
+    display_text(text) {|l| assert_equal link, l}
+  end
+  
+  test 'display text store links' do
+    link1 = 'http://www.google.com'
+    link2 = 'http://www.flickr.com'
+    text = 'click this ' + link1 + ' ' + link2
+    thought = Thought.new :body => text
+    urls = []
+    display_text_store_links(thought, urls)
+    assert_equal 2, urls.size
+  end
+end
