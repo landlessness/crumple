9cb9cbd5519d0a6a37cade858539a69255abd3d7
landlessness <brian@landlessness.net>
2010-09-22 12:01:00 -0700

    oops. too aggressive with editing migrations after deploy.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100922094420) do
+ActiveRecord::Schema.define(:version => 20100922185907) do
 
   create_table "add_ons", :force => true do |t|
     t.string   "name"
