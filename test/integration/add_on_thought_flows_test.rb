5b5831a500568d0fd7c8011eec057f0d40c9ea29
landlessness <brian@landlessness.net>
2010-09-22 11:47:24 -0700

    tests are passing with calls to ActiveResource labs.crumpleapp.com

--- test/integration/add_on_thought_flows_test.rb
+++ test/integration/add_on_thought_flows_test.rb
@@ -3,19 +3,4 @@ require 'test_helper'
 class AddOnThoughtFlowsTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # Replace this with your real tests.
-  test 'get list of music notation thoughts' do
-    get '/music_notation_thoughts/1'
-    # assert_response :success
-    assert t = assigns(:thought)
-    assert_equal 'X:1
-    T:Speed the Plough
-    M:4/4
-    C:Trad.
-    K:G
-    |:GABc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|
-      GABc dedB|dedB dedB|c2ec B2dB|A2F2 G4:|
-    |:g2gf gdBd|g2f2 e2d2|c2ec B2dB|c2A2 A2df|
-      g2gf g2Bd|g2f2 e2d2|c2ec B2dB|A2F2 G4:|', t.body
-  end
 end
--- test/unit/add_on_thought_test.rb
+++ test/unit/add_on_thought_test.rb
@@ -4,15 +4,19 @@ class AddOnThoughtTest < ActiveSupport::TestCase
   def test_should_be_valid
     assert AddOnThought.new.valid?
   end
-  def test_calling_active_resource
-    
+  
+  def test_subclazz
+    subclazz = AddOnThought.subclazz_for('music_notation_thought')
+    thought = subclazz.find 1
+    assert_match /Speed the Plough/, thought.body
   end
+  
   def test_find_by_add_on_and_id
     add_on_element_name = 'music_notation_thought'
     add_on = AddOn.find_by_element_name(add_on_element_name)
     assert add_on.is_a?(AddOn)
     clazz = Class.new(AddOnThought){self.site = add_on.site; self.element_name = add_on.element_name}
-    t = clazz.find(1)
-    assert_equal 'foo', t.body
+    thought = clazz.find(1)
+    assert_match /Speed the Plough/, thought.body
   end
 end
