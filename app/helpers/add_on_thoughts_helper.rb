c713c2c4c7cb94aa0a85bf25b652793c9a3d824e
landlessness <brian@landlessness.net>
2010-10-01 11:52:42 -0700

    using div container instead of iframe for add thoughts show

--- app/helpers/add_on_thoughts_helper.rb
+++ app/helpers/add_on_thoughts_helper.rb
@@ -2,6 +2,10 @@ module AddOnThoughtsHelper
   def add_on_show_url(thought)
     [thought.add_on.site, thought.add_on.element_name.pluralize, thought.add_on_thought_resource_id].join('/') + platform_crumple
   end
+  def render_add_on_show(thought)
+    fetch_url(add_on_show_url(thought)).html_safe
+  end
+
   def add_on_new_url(thought)
     [thought.add_on.site, thought.add_on.element_name.pluralize, 'new'].join('/') + platform_crumple
   end
